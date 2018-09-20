7a8ce341c246c06df581edcb8bca1b7e7f7f0729
Mannu Gupta <abhimanyu98986@gmail.com>
2018-09-20 16:27:03 +0530

    completed lecture 90

--- client/index.js
+++ client/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 import App from './components/App';
+import { cargo } from 'async';
+
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin',
+  },
+  pollInterval: 10000,
+});
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
-})
+});
 
 const Root = () => {
   return (
