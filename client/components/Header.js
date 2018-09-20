90f837d8e0fca33fa6ccbf7a9a26128b023427b8
Mannu Gupta <abhimanyu98986@gmail.com>
2018-09-20 16:49:52 +0530

    completed lecture 92

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import CurrentUser from '../queries/CurrentUser';
-
+import { Link } from 'react-router';
 
 class Header extends Component {
     renderButtons() {
@@ -14,7 +14,14 @@ class Header extends Component {
             return <div> Hi { user.id } Logout </div>;
         } else {
             return (
-                <div> You are not signed in. </div>
+                <div>
+                    <li>
+                        <Link to="/singup"> SignUp</Link>
+                    </li>
+                    <li>
+                        <Link to="/login"> Login</Link>
+                    </li>
+                </div>
             );
         }
     };
@@ -23,7 +30,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    { this.renderButtons() }
+                    <Link to="/" className="brand-logo left">
+                        HOME
+                    </Link>
+                    <ul className="right">
+                        { this.renderButtons() }
+                    </ul>
                 </div>
             </nav>
         );
