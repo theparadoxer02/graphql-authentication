49e1aacca291b0033cd27bf417ea4c037a53f959
Mannu Gupta <abhimanyu98986@gmail.com>
2018-09-21 12:17:58 +0530

    completed lecture 104

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import AuthForm from './AuthForm';
 import { graphql } from 'react-apollo';
 import mutation from '../mutations/Signup';
+import query from '../queries/CurrentUser';
 
 class SignupForm extends Component {
     constructor(props){
@@ -12,6 +13,7 @@ class SignupForm extends Component {
     onSubmit({email, password}) {
         this.props.mutate({
             variables: { email, password },
+            refetchQueries: [{ query }]
         }).catch(res => {
             const errors = res.graphQLErrors.map(error => error.message);
             this.setState({ errors });
